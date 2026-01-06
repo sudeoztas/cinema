1c06e403071555bc02b60edd716d1140d1be7c28
sudeoztas <s26727@pjwstk.edu.pl>
2026-01-06 12:29:46 +0100

    Update Glass3D.cs

--- Glass3D.cs
+++ Glass3D.cs
@@ -5,7 +5,6 @@ namespace CinemaManagementSystem.Items
     [Serializable]
     public class Glass3D : Item
     {
-        // Reusable 3D glasses item
         public bool IsReusable { get; }
 
         public Glass3D(double price, bool isReusable)
